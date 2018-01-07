732b7f4a998d6fe68e4446bad7eb1af012080008
manpreetsingh11 <manpreetsingh000@hotmail.com>
2018-01-07 08:09:42 +0530

    My First Commit

---
+++ CollegeCourse/Controllers/AgentProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeCourse.Controllers
+{
+    public class AgentProfileController : Controller
+    {
+        // GET: AgentProfile
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ CollegeCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeCourse.Startup))]
+namespace CollegeCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
